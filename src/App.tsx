73f29b0844974d8f346705ea11ca721715dbf662
Marcelo Melo <marcelo.melo.ter@estacio.br>
2022-02-28 15:28:18 -0300

    feat: :sparkles: Implement Redux with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Dispatch } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { CountActions } from './redux/actions/countActions';
+import { NameActions } from './redux/actions/nameActions';
+import { AppState } from './redux/reducers/rootReducer';
 
 function App() {
+  const {count} = useSelector((state: AppState) => state.count);
+  const {name} = useSelector((state: AppState) => state.name)
+  const countDispatch = useDispatch<Dispatch<CountActions>>();
+  const nameDispatch = useDispatch<Dispatch<NameActions>>();
+
+  const handleIncrement = () => {
+    countDispatch({type: 'INCREMENT'})
+  }
+
+  const handleDecrement = () => {
+    countDispatch({type: 'DECREMENT'})
+  }
+
+  const handleSetName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    nameDispatch({type: 'SET_NAME', payload: e.target.value})
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={handleIncrement}>+</button>
+        {count}
+        <button onClick={handleDecrement}>-</button>
+      </div>
+      <div>
+        <input type="text" onChange={handleSetName} />
+        {name}
+      </div>
     </div>
   );
 }
